2c0226d8eb43af4b06bc06b00a49882ed8995654
Godwin Goodman Effah <godwinabeaku@gmail.com>
2021-10-29 17:56:11 +0000

    in progress, view done, ts left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ngDynamicInput';
+  // dynamicForm: FormGroup;
+
+  constructor(
+    // private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void { 
+    // this.dynamicForm = this.formBuilder.group({
+    //   name: [ '' ],
+    //   address: [''],
+    // });
+  }
+
+  addRow(event){
+
+  }
+
+
 }
